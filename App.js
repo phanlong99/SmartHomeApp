bfa2e3d617391097f65efdee10eaf2d64f749a35
phanlong99 <phanlong99mq@gmail.com>
2022-01-05 15:32:00 +0700

    Complete Control Livingroom

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Header from './components/Header';
+import Controller from './components/Controller';
+
 export default function App(props) {
   return (
     <View style={styles.container}>
       <Header />
+      <Controller />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flexDirection: 'column',
+  },
 });
